b58da35ab145330572bd1815b36d851b4c76970c
kotlyarDG <kotlyar.d.g@gmail.com>
2021-05-04 20:13:02 +0300

    slider

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 // Проверка, загрузилась ли страница
-$(document).ready(function(){
+$(document).ready(function () {
+
+	$('.third__slider').slick({
+		slidesToShow: 1,
+		dots: true,
+		centerMode: true,
+		centerPadding: '30%',
+		arrows: true,
+		appendDots: '.dots-wrapper'
+	});
 
 });
